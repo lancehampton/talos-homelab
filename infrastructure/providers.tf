9a8f1cd74c07c54af9105c6afb657f11e52dc9a6
Lance Hampton <95756699+lancehampton@users.noreply.github.com>
2025-08-12 21:25:14 -0400

    Add cloudflare and tailscale extensions (#2)
    * add basic talos config
    
    * add remote backend
    
    * add cloudflare and tailscale extensions

---
+++ infrastructure/providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    talos = {
+      source  = "siderolabs/talos"
+      version = ">= 0.4.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.8"
+    }
+  }
+  required_version = ">= 1.6.0"
+
+  # https://developers.cloudflare.com/terraform/advanced-topics/remote-backend/
+  backend "s3" {
+    bucket                      = var.r2_bucket_name
+    key                         = "talos/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    access_key                  = var.r2_access_key_id
+    secret_key                  = var.r2_secret_access_key
+    endpoints                   = { s3 = var.r2_endpoint }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
--- infrastructure/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    talos = {
-      source  = "siderolabs/talos"
-      version = ">= 0.4.0"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
